54118407ee377b19d4fc82982d8141737a0f0d7e
Pratham Virani <25014234@imail.sunway.edu.my>
2026-08-20 00:17:26 +0800

    Calculate connecting-rod ratio in Engine Lab state

--- Game/Assets/VES/Core/ICE/EngineCalculator.cs
+++ Game/Assets/VES/Core/ICE/EngineCalculator.cs
@@ -9,8 +9,15 @@ namespace VehicleEngineeringSandbox.Core.ICE
             var clearancePerCylinderM3 = EngineGeometry.ClearanceVolumeFromCompressionRatio(sweptPerCylinderM3, configuration.CompressionRatio);
             var meanPistonSpeedMps = EngineGeometry.MeanPistonSpeedMps(configuration.StrokeM, configuration.EngineSpeedRpm);
             var boreStrokeRatio = configuration.BoreM / configuration.StrokeM;
+            var rodStrokeRatio = configuration.ConnectingRodLengthM / configuration.StrokeM;
 
-            return new EngineCalculatedState(totalDisplacementM3, sweptPerCylinderM3, clearancePerCylinderM3, meanPistonSpeedMps, boreStrokeRatio);
+            return new EngineCalculatedState(
+                totalDisplacementM3,
+                sweptPerCylinderM3,
+                clearancePerCylinderM3,
+                meanPistonSpeedMps,
+                boreStrokeRatio,
+                rodStrokeRatio);
         }
     }
 }
