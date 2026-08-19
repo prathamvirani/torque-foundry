a328a49f6d9efc40664acd40429942dd7cec4456
Pratham Virani <25014234@imail.sunway.edu.my>
2026-08-20 00:10:07 +0800

    Add engine calculator

---
+++ Game/Assets/VES/Core/ICE/EngineCalculator.cs
@@ -0,0 +1,16 @@
+namespace VehicleEngineeringSandbox.Core.ICE
+{
+    public static class EngineCalculator
+    {
+        public static EngineCalculatedState Calculate(EngineConfiguration configuration)
+        {
+            var totalDisplacementM3 = EngineGeometry.TotalDisplacementM3(configuration.BoreM, configuration.StrokeM, configuration.CylinderCount);
+            var sweptPerCylinderM3 = totalDisplacementM3 / configuration.CylinderCount;
+            var clearancePerCylinderM3 = EngineGeometry.ClearanceVolumeFromCompressionRatio(sweptPerCylinderM3, configuration.CompressionRatio);
+            var meanPistonSpeedMps = EngineGeometry.MeanPistonSpeedMps(configuration.StrokeM, configuration.EngineSpeedRpm);
+            var boreStrokeRatio = configuration.BoreM / configuration.StrokeM;
+
+            return new EngineCalculatedState(totalDisplacementM3, sweptPerCylinderM3, clearancePerCylinderM3, meanPistonSpeedMps, boreStrokeRatio);
+        }
+    }
+}
